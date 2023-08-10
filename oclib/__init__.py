946d263886de99dc473bf421a7538d41c08f94cc
oceanection <oceanection@gmail.com>
2023-08-11 07:51:00 +0900

    main

--- oclib/__init__.py
+++ oclib/__init__.py
@@ -33,16 +33,20 @@ def eval_loss(loader, device, net, criterion):
     return loss
 
 def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device, history):
-    base_epochs = len(history)
+    # tqdmライブラリのインポート
+    from tqdm.notebook import tqdm
 
+    base_epochs = len(history)
+  
     for epoch in range(base_epochs, num_epochs+base_epochs):
-        # 1エポックあたりの正解数（精度計算用）
+        # 1エポックあたりの正解数(精度計算用)
         n_train_acc, n_val_acc = 0, 0
-        # 1エポックあたりの累積損失（平均化前）
+        # 1エポックあたりの累積損失(平均化前)
         train_loss, val_loss = 0, 0
         # 1エポックあたりのデータ累積件数
         n_train, n_test = 0, 0
 
+        #訓練フェーズ
         net.train()
 
         for inputs, labels in tqdm(train_loader):
@@ -50,35 +54,35 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
             train_batch_size = len(labels)
             # 1エポックあたりのデータ累積件数
             n_train += train_batch_size
+    
+            # GPUヘ転送
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
+            # 勾配の初期化
+            optimizer.zero_grad()
 
-        # GPU転送
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+            # 予測計算
+            outputs = net(inputs)
 
-        # 勾配初期化
-        optimizer.zero_grad()
+            # 損失計算
+            loss = criterion(outputs, labels)
 
-        # 予測計算
-        outputs = net(inputs)
+            # 勾配計算
+            loss.backward()
 
-        # 損失計算
-        loss = criterion(outputs, labels)
+            # パラメータ修正
+            optimizer.step()
 
-        # 勾配計算
-        loss.backward()
+            # 予測ラベル導出
+            predicted = torch.max(outputs, 1)[1]
 
-        # パラメータ修正
-        optimizer.step()
+            # 平均前の損失と正解数の計算
+            # lossは平均計算が行われているので平均前の損失に戻して加算
+            train_loss += loss.item() * train_batch_size 
+            n_train_acc += (predicted == labels).sum().item() 
 
-        # 予測ラベル導出
-        predicted = torch.max(outputs, 1)[1]
-
-        # 平均前の損失と正解数の計算
-        # lossは平均計算が行われているので平均前の損失に戻して加算
-        train_loss += loss.item() * train_batch_size
-        n_train_acc += (predicted == labels).sum().item()
-
+        #予測フェーズ
         net.eval()
 
         for inputs_test, labels_test in test_loader:
@@ -87,7 +91,7 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
             # 1エポックあたりのデータ累積件数
             n_test += test_batch_size
 
-            # GPU転送
+            # GPUヘ転送
             inputs_test = inputs_test.to(device)
             labels_test = labels_test.to(device)
 
@@ -96,6 +100,14 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
 
             # 損失計算
             loss_test = criterion(outputs_test, labels_test)
+ 
+            # 予測ラベル導出
+            predicted_test = torch.max(outputs_test, 1)[1]
+
+            #  平均前の損失と正解数の計算
+            # lossは平均計算が行われているので平均前の損失に戻して加算
+            val_loss +=  loss_test.item() * test_batch_size
+            n_val_acc +=  (predicted_test == labels_test).sum().item()
 
         # 精度計算
         train_acc = n_train_acc / n_train
@@ -108,9 +120,7 @@ def fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device
         # 記録
         item = np.array([epoch+1, avg_train_loss, train_acc, avg_val_loss, val_acc])
         history = np.vstack((history, item))
-
     return history
-
 
 # 学習ログ解析
 def evaluate_history(history):
