12b6e25d4b261da1b3e5806277d81b0cdaeabdc5
oceanection <oceanection@gmail.com>
2023-08-14 17:23:37 +0900

    main

--- ocmodel/__init__.py
+++ ocmodel/__init__.py
@@ -2,36 +2,8 @@ import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self, n_output, n_hidden):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3)
-        self.conv2 = nn.Conv2d(32, 32, 3)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d((2, 2))
-        self.flatten = nn.Flatten()
-        self.l1 = nn.Linear(6272, n_hidden)
-        self.l2 = nn.Linear(n_hidden, n_output)
-
-        self.features = nn.Sequential(
-            self.conv1,
-            self.relu,
-            self.conv2,
-            self.maxpool
-        )
-
-        self.classifier = nn.Sequential(
-            self.l1,
-            self.relu,
-            self.l2
-        )
-
-    def forward(self, x):
-        x1 = self.features(x)
-        x2 = self.flatten(x1)
-        x3 = self.classifier(x2)
-        return x3
-
-class CNN_v2(nn.Module):
+    """
+    """
     def __init__(self, num_classes):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, 3, padding=(1, 1))
@@ -45,6 +17,9 @@ class CNN_v2(nn.Module):
         self.maxpool = nn.MaxPool2d((2, 2))
         self.l1 = nn.Linear(4*4*128, 128)
         self.l2 = nn.Linear(128, num_classes)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dropout2 = nn.Dropout(0.3)
+        self.dropout3 = nn.Dropout(0.4)
 
         self.features = nn.Sequential(
             self.conv1,
@@ -52,20 +27,24 @@ class CNN_v2(nn.Module):
             self.conv2,
             self.relu,
             self.maxpool,
+            self.dropout1,
             self.conv3,
             self.relu,
             self.conv4,
             self.relu,
             self.maxpool,
+            self.dropout2,
             self.conv5,
             self.relu,
             self.conv6,
-            self.maxpool
+            self.maxpool,
+            self.dropout3,
         )
 
         self.classifier = nn.Sequential(
             self.l1,
             self.relu,
+            self.dropout3,
             self.l2
         )
 
